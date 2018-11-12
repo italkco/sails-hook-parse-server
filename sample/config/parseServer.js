913e34b92ccc4cf3c8299716bcb602cadf2abace
Matija Stepanic <stepanic@italk.hr>
2018-11-12 08:53:57 +0100

    Parse Server init default config in Sails.js app

--- sample/config/parseServer.js
+++ sample/config/parseServer.js
@@ -1,3 +1,11 @@
-module.exports.parseServer2 = {
-  
+module.exports.parseServer = {
+  mountPoint: '/parse',
+  parseServerConfig: {
+    databaseURI: 'mongodb://localhost:27017/dev', // Connection string for your MongoDB database
+    cloud: '/home/myApp/cloud/main.js', // Absolute path to your Cloud Code
+    appId: 'myAppId',
+    masterKey: 'myMasterKey', // Keep this key secret!
+    fileKey: 'optionalFileKey',
+    serverURL: 'http://localhost:1337/parse' // Don't forget to change to https if needed
+  }
 }
